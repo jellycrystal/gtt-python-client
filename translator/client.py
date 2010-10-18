2fe3a1d5113cf9f1f03860a872c6db2844cfb243
Yury Yurevich <yyurevich@jellycrystal.com>
2010-10-18 11:04:08 +0700

    Fix update acl.

--- translator/client.py
+++ translator/client.py
@@ -16,6 +16,7 @@ http://code.google.com/apis/gtt/
 __author__ = 'yyurevich@jellycrystal.com (Yury Yurevich)'
 
 import urllib
+import atom.http_core
 import gdata.client
 import gdata.acl.data
 import translator.data as tdata
@@ -41,7 +42,27 @@ def _provide_url(id_or_url):
         return '%s/%s' % (DOCUMENTS_URL, urllib.quote(id_or_url))
 
 
-class TranslatorToolkitClient(gdata.client.GDClient):
+class PutEntryGDClient(gdata.client.GDClient):
+    """
+    GDClient with .put_entry() written in similar way to .post().
+    """
+
+    def put_entry(self, entry, uri, auth_token=None, converter=None,
+                  desired_class=None, **kwargs):
+        if converter is None and desired_class is None:
+            desired_class = entry.__class__
+        http_request = atom.http_core.HttpRequest()
+        http_request.add_body_part(
+            entry.to_string(gdata.client.get_xml_version(self.api_version)),
+            'application/atom+xml')
+        return self.request(method='PUT', uri=uri, auth_token=auth_token,
+                            http_request=http_request, converter=converter,
+                            desired_class=desired_class, **kwargs)
+
+    PutEntry = put_entry
+
+
+class TranslatorToolkitClient(PutEntryGDClient):
     api_version = '1.0'
     auth_service = 'gtrans'
     auth_scopes = [AUTH_SCOPE]
@@ -151,13 +172,13 @@ class TranslatorToolkitClient(gdata.client.GDClient):
         scope = gdata.acl.data.AclScope(type='user', value=user_email)
         new_role = gdata.acl.data.AclRole(value=new_role)
         entry = gdata.acl.data.AclEntry(role=new_role, scope=scope)
-        link = '%s/%s' % (acl_link, user)
-        return self.put(entry, link, auth_token=auth_token, **kwargs)
+        link = '%s/%s' % (acl_link, urllib.quote(user_email))
+        return self.put_entry(entry, link, auth_token=auth_token, **kwargs)
 
     UpdateAcl = update_acl
 
     def delete_acl(self, acl_link, user_email, auth_token=None, **kwargs):
-        link = '%s/%s' % (acl_link, user_email)
+        link = '%s/%s' % (acl_link, urllib.quote(user_email))
         return self.delete(link)
 
     DeleteAcl = delete_acl
