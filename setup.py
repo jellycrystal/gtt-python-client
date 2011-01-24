edeb8c3a1005afc04629dd7fc879ddab902fb22f
Yury Yurevich <yyurevich@jellycrystal.com>
2011-01-24 16:23:59 +0600

    Update version to 0.1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gtt-python-client',
-    version='0.1',
+    version='0.1.2',
     description='Python bindings for Google Translator Toolkit API.',
     long_description=open('README.md').read(),
     author='Yury Yurevich',
